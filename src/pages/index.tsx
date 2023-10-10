c7a8eb8cdc74e4c038f2d68629161f5a841a7111
Roman lesyo <ladiginscormag@gmail.com>
2023-10-10 15:27:32 +0300

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,7 +10,7 @@ export default function Home() {
    return (
       <>
          <Head>
-            <title>Resume</title>
+            <title>Portfolio</title>
             <meta name='description' content='This is resume of Serafim Barey' />
             <meta name='viewport' content='width=device-width, initial-scale=1' />
             <link rel='icon' href='./favicon.png' />
