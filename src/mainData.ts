8bd5baa76f0dd72140829e833846b195b11df73e
Roman lesyo <98737355+Romaleso261981@users.noreply.github.com>
2024-04-17 21:39:33 +0300

    added work

--- src/mainData.ts
+++ src/mainData.ts
@@ -5,7 +5,7 @@ export const mainData: MainPropsType["mainData"] = [
     id: 1,
     title: "Про мене",
     description:
-      "Я зацікавився програмуванням, щоб створити свій сайт для власного бізнесу, а потім продовжив вивчати нові технології. У мене пів року досвіду роботи над проектом у SoftServe стажування та роботи над моїм улюбленим проектом. Маю більше року досвіду на freelance, займався виготовленням сайтів (Сайти візитка, інтернет магазин). Мої сильні сторони - комунікабельність і наполегливість. Я прагну розвиватися як Full stack developer, досягаючи цілей. Здатний швидко навчатися, знаходити рішення та брати на себе відповідальність, я можу працювати з JavaScript(ES6+), TypeScript, HTML5, CSS3, React, Redux, Next.js, Node.js, Express, MongoDB, Figma. Шукаю сильну команду та наставника для розробки нових проектів. Крім того, будучи перфекціоністом протягом усього життя, я прагну писати чистий, зручний, ефективний код навіть у скрутному середовищі. Я прагну дотримуватись загальних принципів програмування, таких як SOLID, KISS і DRY.",
+      "Я зацікавився програмуванням, щоб створити свій сайт для власного бізнесу, а потім продовжив вивчати нові технології. У мене пів року досвіду роботи над проектом у SoftServe стажування та роботи над моїм улюбленим проектом. Маю більше року досвіду на freelance, займався виготовленням сайтів (Сайти візитка, інтернет магазин). Мої сильні сторони - комунікабельність і наполегливість. Я прагну розвиватися як Full stack developer, досягаючи цілей. Здатний швидко навчатися, знаходити рішення та брати на себе відповідальність, я можу працювати з JavaScript(ES6+), TypeScript, HTML5, CSS3, React, Redux, Next.js, Node.js, Express, MongoDB, Figma. Шукаю сильну команду та наставника для розробки нових проектів. Крім того, будучи перфекціоністом протягом усього життя, я прагну писати чистий, зручний, ефективний код навіть у скрутному середовищі. Я прагну дотримуватись загальних принципів програмування, таких як SOLID, KISS і DRY."
     // title: "About me",
     // description:
     //  "I became interested in programming to create my website for own business and than continued to learn new technologies. I have half a year of experience on a project at SoftServe intership and working on my pet project. My strengths are communication and persistence. I strive to develop as a Full stack developer, achieving goals. Able to learn quickly, find solutions and take responsibility, I can work with JavaScript(ES6+), TypeScript, HTML5, CSS3, React, Redux, Next.js, Node.js, Express, MongoDB, Figma. I am looking for a strong teamand mentor to develop new projects. Additionally, being a lifelong perfectionist, I aim to write clean, eliable,  efficient code even in a time pressured environment. I strive to follow general programming principles such as SOLID, KISS, and DRY.",
@@ -24,13 +24,13 @@ export const mainData: MainPropsType["mainData"] = [
       { id: 3, src: "/images/javascript.svg" },
       { id: 4, src: "/images/react.svg" },
       { id: 5, src: "/images/nextjs.svg" },
-      { id: 6, src: "/images/ts.svg" },
-    ],
+      { id: 6, src: "/images/ts.svg" }
+    ]
   },
   {
     id: 3,
     title: "Рівень англійської",
-    description: `Мій рівень англійської pre-intermediate.`,
+    description: `Мій рівень англійської pre-intermediate.`
     // title: "English level",
     // description: `My level of English is pre-intermediate.`,
   },
@@ -38,7 +38,7 @@ export const mainData: MainPropsType["mainData"] = [
     id: 4,
     title: "Досвід роботи",
     description:
-      "Я створив сайти: ladclimatservice.com.ua на WordPress deshevakovka.com.ua на HTML5, CSS3, JavaScript(ES6+) remontonline.netlify.app на React, Redux, Node.js, Next.js, Express, MongoDB. Основним завданням було впровадження нових функцій і оновлення поточного коду за допомогою сучасних технологій і стеків. Вивчав і використовував стек React-Router, Redux-Toolkit, React Hooks, MongoDB, CRUD, AJAX, JSON, REST API і працював над проектом постійно та понаднормово вдома 3-5 годин на день.",
+      "Я створив сайти: ladclimatservice.com.ua на WordPress deshevakovka.com.ua на HTML5, CSS3, JavaScript(ES6+) remontonline.netlify.app на React, Redux, Node.js, Next.js, Express, MongoDB. Основним завданням було впровадження нових функцій і оновлення поточного коду за допомогою сучасних технологій і стеків. Вивчав і використовував стек React-Router, Redux-Toolkit, React Hooks, MongoDB, CRUD, AJAX, JSON, REST API і працював над проектом постійно та понаднормово вдома 3-5 годин на день."
     // description:
     //  "I created websites: ladclimatservice.com.ua by using WordPress deshevakovka.com.ua by HTML5, CSS3, JavaScript(ES6+) remontonline.netlify.app by React, Redux, Node.js, Next.js, Express, MongoDB. The main task was to introduce new features and update the current code using modern technologies and stacks. Studied and used the stack React-Router, Redux-Toolkit, React Hooks, MongoDB, CRUD, AJAX, JSON, REST API and worked on the project ull time and overtime at home 3-5 hours a day.",
   },
@@ -52,8 +52,8 @@ export const mainData: MainPropsType["mainData"] = [
       { id: 3, skill: "Комунікабельність" },
       { id: 4, skill: "Виконливість" },
       { id: 5, skill: "Надійність" },
-      { id: 6, skill: "Управління часом" },
-    ],
+      { id: 6, skill: "Управління часом" }
+    ]
     // description: "I can highlight the following soft skills:",
     // listItems: [
     //  { id: 1, skill: "Honesty" },
@@ -80,9 +80,9 @@ export const mainData: MainPropsType["mainData"] = [
           { id: 2, technology: "JavaScript" },
           { id: 3, technology: " Node.js" },
           { id: 4, technology: " MongoDB" },
-          { id: 5, technology: " Express" },
+          { id: 5, technology: " Express" }
         ],
-        src: "https://pizza-storr.netlify.app",
+        src: "https://pizza-storr.netlify.app"
       },
       {
         id: 1,
@@ -94,9 +94,9 @@ export const mainData: MainPropsType["mainData"] = [
           { id: 2, technology: "JavaScript" },
           { id: 3, technology: " Node.js" },
           { id: 4, technology: " MongoDB" },
-          { id: 5, technology: " Express" },
+          { id: 5, technology: " Express" }
         ],
-        src: "https://remontonline.netlify.app/",
+        src: "https://remontonline.netlify.app/"
       },
       {
         id: 2,
@@ -109,9 +109,9 @@ export const mainData: MainPropsType["mainData"] = [
           { id: 2, technology: "CSS" },
           { id: 3, technology: " Node.js" },
           { id: 4, technology: " MongoDB" },
-          { id: 5, technology: " Express" },
+          { id: 5, technology: " Express" }
         ],
-        src: "https://viacheslav-tykhovlis.github.io/petly-site/",
+        src: "https://viacheslav-tykhovlis.github.io/petly-site/"
       },
       {
         id: 3,
@@ -124,9 +124,9 @@ export const mainData: MainPropsType["mainData"] = [
           { id: 2, technology: " JavaScript(ES6+)" },
           { id: 3, technology: "CSS3" },
           { id: 4, technology: "Webpack" },
-          { id: 5, technology: "Babel" },
+          { id: 5, technology: "Babel" }
         ],
-        src: "https://deshevakovka.com.ua/",
+        src: "https://deshevakovka.com.ua/"
       },
       {
         id: 4,
@@ -139,9 +139,9 @@ export const mainData: MainPropsType["mainData"] = [
           { id: 2, technology: "HTML" },
           { id: 3, technology: "CSS" },
           { id: 4, technology: "Webpack" },
-          { id: 5, technology: "Babel" },
+          { id: 5, technology: "Babel" }
         ],
-        src: "https://ladclimatservice.com.ua/",
+        src: "https://ladclimatservice.com.ua/"
       },
       {
         id: 5,
@@ -153,9 +153,9 @@ export const mainData: MainPropsType["mainData"] = [
           { id: 2, technology: "HTML" },
           { id: 3, technology: "CSS" },
           { id: 4, technology: "Webpack" },
-          { id: 5, technology: "Babel" },
+          { id: 5, technology: "Babel" }
         ],
-        src: "https://l-coffee-shop.netlify.app",
+        src: "https://l-coffee-shop.netlify.app"
       },
       {
         id: 6,
@@ -167,9 +167,9 @@ export const mainData: MainPropsType["mainData"] = [
           { id: 2, technology: "HTML" },
           { id: 3, technology: "CSS" },
           { id: 4, technology: "Webpack" },
-          { id: 5, technology: "Babel" },
+          { id: 5, technology: "Babel" }
         ],
-        src: "https://our-chat-my.netlify.app",
+        src: "https://our-chat-my.netlify.app"
       },
       {
         id: 7,
@@ -181,9 +181,9 @@ export const mainData: MainPropsType["mainData"] = [
           { id: 2, technology: "HTML" },
           { id: 3, technology: "CSS" },
           { id: 4, technology: "Webpack" },
-          { id: 5, technology: "Babel" },
+          { id: 5, technology: "Babel" }
         ],
-        src: "https://stunning-strudel-576147.netlify.app",
+        src: "https://stunning-strudel-576147.netlify.app"
       },
       {
         id: 8,
@@ -195,36 +195,48 @@ export const mainData: MainPropsType["mainData"] = [
           { id: 2, technology: "HTML" },
           { id: 3, technology: "CSS" },
           { id: 4, technology: "Webpack" },
-          { id: 5, technology: "Babel" },
+          { id: 5, technology: "Babel" }
         ],
-        src: "https://flowers-goithop.netlify.app",
+        src: "https://flowers-goithop.netlify.app"
       },
-    ],
+      {
+        id: 9,
+        imageSrc: "/Coffee-Shop-1024x765.jpg",
+        projectTitle: "Coffee shop website",
+        projectDescription:
+          "Лендінг по продажу кави на HTML5, CSS3, JavaScript(ES6+)",
+        technologies: [
+          { id: 2, technology: "HTML" },
+          { id: 3, technology: "CSS" },
+          { id: 4, technology: "JavaScript" }
+        ],
+        src: "https://coffee-team-project-owner.vercel.app/"
+      }
+    ]
   },
   {
     id: 7,
     title: "Освіта",
-    description:
-      "",
+    description: "",
     listItems: [
       {
         id: 1,
         skill:
-          "Львівський Державний Аграрний університет Автоматизація сільськогосподарського виробництва Механік-інженер 2021 - 2025",
+          "Львівський Державний Аграрний університет Автоматизація сільськогосподарського виробництва Механік-інженер 2021 - 2025"
       },
       {
         id: 2,
-        skill: "Lemon.School Java Developer Грудень 2021 - Травень 2021",
+        skill: "Lemon.School Java Developer Грудень 2021 - Травень 2021"
       },
       {
         id: 3,
         skill:
-          "Soft Serve Academy Front-End Developer Травень 2020 - Грудень 2020",
+          "Soft Serve Academy Front-End Developer Травень 2020 - Грудень 2020"
       },
       {
         id: 4,
-        skill: "GOIT JavaScript Developer Лютий  2021 - Грудень 2022",
-      },
-    ],
-  },
+        skill: "GOIT JavaScript Developer Лютий  2021 - Грудень 2022"
+      }
+    ]
+  }
 ];
